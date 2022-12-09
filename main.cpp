4f017fa38acdd1d69e7a928f73a19dc90ef044eb
Jeon <dbs_dmswl92@naver.com>
2022-12-09 17:57:43 +0900

    ADD window standard

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 #include<Windows.h>
 
+const wchar_t gClassName[]{ L"MyWindowClass" };
+const wchar_t gTitle[]{ L"Direct3D" };
+const int WINDOW_WIDTH{ 800 };
+const int WINDOW_HEIGHT{ 600 };
+
+HWND gHwnd{ };
+HINSTANCE gInstance{ };
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
